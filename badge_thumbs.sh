cfb6fe94f39fb4abed597bade48c095594f7a71a
Christoph Roeper <christoph@calvin.yukon>
2019-03-18 18:15:12 +0100

    UPD: badge_thumbs script for public and confidential

--- badge_thumbs.sh
+++ badge_thumbs.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-project_base_path="/home/christoph/Projects/bizcard/roeper.biz/public/assets/site"
+project_base=${2:-public}
+
+if [ "${project_base}" = "confidential" ]; then
+  project_base_path="/home/christoph/Projects/bizcard/roeper.biz/${project_base}/cv/assets/site"
+else
+  project_base_path="/home/christoph/Projects/bizcard/roeper.biz/${project_base}/assets/site"
+fi
 
 badge_path="${project_base_path}/badges"
 thumbs_path="${project_base_path}/images/thumbnails"
